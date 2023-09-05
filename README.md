1669f0c81ed3ccd7401814bba40b2ba907e5d73e
Josan Ruben Garcia Castillo <Josan.Castillo@ibm.com>
2023-09-05 14:53:04 -0600

    v1.2
    Se agregan estilos, value, archivos properties y redirect y forward.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CursoSpring
 Aplicación construida con Spring del curso Spring Framework &amp; Spring Boot desde cero a experto
+
+Este es el primer proyecto del curso, termino en v1.2
--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/app/springbootweb/controllers/Index_Controller.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/app/springbootweb/controllers/Index_Controller.java
@@ -2,6 +2,8 @@ package com.bolsadeideas.springboot.app.springbootweb.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,10 +15,19 @@ import org.springframework.ui.Model;
 @RequestMapping("/app")
 public class Index_Controller {
 
+    @Value("${texto.indexcontroller.index.titulo}")
+    private String textoIndex;
+
+    @Value("${texto.indexcontroller.perfil.titulo}")
+    private String textoPerfil;
+
+    @Value("${texto.indexcontroller.listar.titulo}")
+    private String textoListar;
+
     @GetMapping({"/index", "/", "", "/home"})
     public String index(Model model){
         
-        model.addAttribute("titulo", "Hola Spring Framework");
+        model.addAttribute("titulo", textoIndex);
         
         return "index";
     }
@@ -29,7 +40,7 @@ public class Index_Controller {
         usuario.setApellido("Castillo");
         usuario.setEmail("Josan.Castillo@ibm.com");
         model.addAttribute("usuario", usuario);
-        model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+        model.addAttribute("titulo", textoPerfil.concat(usuario.getNombre()));
         
         return "perfil";
     }
@@ -37,7 +48,7 @@ public class Index_Controller {
     @GetMapping("/listar")
     public String listar(Model model){
 
-        model.addAttribute("titulo", "Listado de usuarios");
+        model.addAttribute("titulo", textoListar);
 
         return "listar";
     }
